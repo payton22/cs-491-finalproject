9eba8cf13e3743b71df064c2e31b0a023c36fd46
Payton <pjknoch@sbcglobal.net>
2021-04-25 04:00:06 -0700

    Configured AWS2

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source /home/Documents/GitHub/cs-491-finalproject/venv/bin/activate
 pyinstaller --onefile /home/Documents/GitHub/cs-491-finalproject/source/tictactoe.py
-aws s3 mv /var/jenkins_home/workspace/final-project/dist/tictactoe s3://activity11bucket
+aws s3 mv /var/jenkins_home/workspace/cs491finalproject/dist/tictactoe s3://activity11bucket
